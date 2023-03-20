5bf1351bfe175909b495c92bf3057b9b14d61bba
TejaVenkatBalla <tejavenkatballa@gmail.com>
2023-03-20 19:08:50 +0530

    change nav bar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -3,22 +3,26 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg ">
 
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                {/* <a className="navbar-brand" href="#">Navbar</a> */}
+                <img  style={{height:"50px"}} src='http://store-images.s-microsoft.com/image/apps.29097.13988821287716003.a94a0249-6958-4773-a899-618ac4ac17e2.cfbccc9c-52b0-4e6c-9c05-2b62257191a1' alt='logo'></img>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    <a className="nav-link active" aria-current="page" href="https://www.virustotal.com/gui/home/upload">Home</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
+                    <a className="nav-link" href="https://developers.virustotal.com/reference/overview">API</a>
                     </li>
-                    <li className="nav-item dropdown">
+                    <li className="nav-item">
+                    <a className="nav-link" href="https://www.virustotal.com/graph/">Graph</a>
+                    </li>
+                    {/* <li className="nav-item dropdown">
                     <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Dropdown
                     </a>
@@ -28,10 +32,10 @@ function Navbar() {
                         <li><hr className="dropdown-divider"/></li>
                         <li><a className="dropdown-item" href="#">Something else here</a></li>
                     </ul>
-                    </li>
-                    <li className="nav-item">
+                    </li> */}
+                    {/* <li className="nav-item">
                     <a className="nav-link disabled">Disabled</a>
-                    </li>
+                    </li> */}
                 </ul>
                 <form className="d-flex" role="search">
                     <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
